d8d5c26fb65eae14f8fb9114d26003047b64b9ce
JJoe <48448023+JJoeDev@users.noreply.github.com>
2024-01-30 10:13:37 +0100

    GRID
    GRID BEGINNING

--- Eventyrspil 2/Assets/Scripts/mapGrid.cs
+++ Eventyrspil 2/Assets/Scripts/mapGrid.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class mapGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera cam;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridHeight;
+    [SerializeField] private float gridCellSize;
+    [SerializeField] private float cellOffset;
+
+    [SerializeField] private GameObject cellPrefab;
+
+    private void Start()
     {
-        
+        float orthSize = cam.orthographicSize;
+
+        float left = (orthSize / (Screen.height / 2)) * (Screen.width / 2);
+
+        for(int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                Vector3 pos = new Vector3(i * gridCellSize, j * gridCellSize, 0);
+                pos.x -= left - cellOffset;
+                pos.y -= orthSize - cellOffset;
+                Instantiate(cellPrefab, pos, Quaternion.identity);
+            }
+        }
     }
 }
