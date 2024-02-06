b122e6adc2c1f2e6ef90e8600c8b6a63ccd2118e
thom989t <thomas.halgaard.ditlevsen@gmail.com>
2024-02-06 10:12:20 +0100

    health interface

---
+++ Eventyrspil 2/Assets/Scripts/heks.cs
@@ -0,0 +1,14 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heks : IHealth
+{
+    public int Health { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+    public int Damage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
