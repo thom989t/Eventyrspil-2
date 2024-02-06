2c9872a8c0182052402e4866051065ba7c0f51f8
JJoe <48448023+JJoeDev@users.noreply.github.com>
2024-02-06 10:15:49 +0100

    Baggrund

--- Eventyrspil 2/Assets/Scripts/mapGrid.cs
+++ Eventyrspil 2/Assets/Scripts/mapGrid.cs
@@ -4,11 +4,15 @@ public class mapGrid : MonoBehaviour
 {
     [SerializeField] private Camera cam;
 
+    [Header("General Grid")]
     [SerializeField] private int gridWidth;
     [SerializeField] private int gridHeight;
     [SerializeField] private float gridCellSize;
     [SerializeField] private float cellOffset;
     [SerializeField] private float leftOffset;
+
+    [Header("Lawn mowers")]
+    [SerializeField] private Color mowerColor;
 
     [SerializeField] private GameObject cellPrefab;
 
@@ -16,7 +20,7 @@ public class mapGrid : MonoBehaviour
     {
         float orthSize = cam.orthographicSize;
 
-        float left = (orthSize / (Screen.height / 2)) * (Screen.width / 2);
+        float left = orthSize / (Screen.height / 2) * (Screen.width / 2);
 
         for(int i = 0; i < gridWidth; i++)
         {
@@ -25,7 +29,12 @@ public class mapGrid : MonoBehaviour
                 Vector3 pos = new Vector3(i * gridCellSize, j * gridCellSize, 0);
                 pos.x -= left - cellOffset - leftOffset;
                 pos.y -= orthSize - cellOffset;
-                Instantiate(cellPrefab, pos, Quaternion.identity);
+                GameObject spot = Instantiate(cellPrefab, pos, Quaternion.identity);
+
+                if(i == 0)
+                {
+                    spot.GetComponent<SpriteRenderer>().color = mowerColor;
+                }
             }
         }
     }
