d870b901207878735443aa25376c9f1e4b2b499c
JJoe <48448023+JJoeDev@users.noreply.github.com>
2024-03-19 08:58:44 +0100

    pus

---
+++ Eventyrspil 2/Assets/Scripts/gameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject tower;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider == null) return;
+
+            if (hit.collider.CompareTag("Grid"))
+            {
+                Transform tileT = hit.collider.transform;
+                Instantiate(tower, tileT.position, Quaternion.identity);
+            }
+        }
+    }
+}
